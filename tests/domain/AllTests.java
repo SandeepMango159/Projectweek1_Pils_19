f4711b15c0802e36aeadbe6a00e7c86466c1930a
Jarne Schreppers <jarne.schreppers@student.ucll.be>
2021-03-30 16:09:28 +0200

    AllTests aangepast

--- tests/domain/AllTests.java
+++ tests/domain/AllTests.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, RechthoekTest.class, LijnstukTest.class, DriehoekTest.class, OmhullendeTest.class, TekeningTest.class, WoordenLijstTest.class})
+@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, RechthoekTest.class, LijnstukTest.class, DriehoekTest.class, OmhullendeTest.class,
+        TekeningTest.class, WoordenLijstTest.class, HintLetterTest.class, HintWoordTest.class})
 public class AllTests {
 
     // Geen constructor nodig of iets, gewoon runnen en dan runt die alles achter @SuiteClasses
